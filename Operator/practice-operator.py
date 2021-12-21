5100ccc154e515524114bdba2e628b8c0defe993
Kyeongjun <v49011591@gmail.com>
2021-12-21 21:57:58 +0900

    연산자 커밋

---
+++ Operator/practice-operator.py
@@ -0,0 +1,12 @@
+print(1+1)
+print(3-1)
+print(5*2)
+print(6/3)
+print(2**3) # 2^3
+print(5%3) # 나머지 구하기
+print(5//3) # 몫 구하기
+
+
+print((3>0) and (3<6)) # True
+print((3>0) and (3>6)) # False
+print((3>0) or (3>5)) # True
